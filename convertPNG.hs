ad8aaeb6e68646b57c726b5b7cb011486d57bcdb
Leif <lgrele@gmail.com>
2013-09-05 12:02:52 -0700

    Implemented initial config file parsing.
    It's a huge mess and there are no tests for it yet though.

--- convertPNG.hs
+++ convertPNG.hs
@@ -1,16 +1,18 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses, DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses,
+DeriveDataTypeable, OverloadedStrings, StandaloneDeriving #-}
 
 import qualified Data.Map.Strict as M
+import Control.Applicative((<$>),(<*>), empty)
+import Numeric
 import Data.Maybe
 import Data.Either
 import Data.Tuple
 import Data.Aeson
 import Data.List(unzip4)
 import Codec.Picture.Png(decodePng)
---import Codec.Picture.Types(DynamicImage(..),Pixel(..),PixelRGBA16(..),Image(..),
---                           ColorConvertible(..),pixelFold,promotePixel)
 import Codec.Picture.Types
-import qualified Data.ByteString as L
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as B
 import System.Environment(getArgs)
 import System.Console.CmdLib
 import Control.Monad
@@ -25,7 +27,6 @@ instance Attributes Main where
     attributes _ = group "Options" [
         start          %> [ Help "Start position of the macro.",
                             ArgHelp "(X,Y)",
-                            Default "0,0",
                             Short ['s'],
                             Long ["start"] ],
         input          %> [ Help "Images to be converted to blueprints.",
@@ -39,7 +40,6 @@ instance Attributes Main where
         phases         %> [ Help "Phase to create a blueprint for.",
                             ArgHelp "[All|Dig|Build|Place|Query]",
                             Long ["phase"],
-                            Default "All",
                             Short ['p'] ],
         singlePhasePNG %> [ Help "If True, a pixel represents a single instruction. If False \
                                  \a pixel represents an instruction for each phase",
@@ -57,57 +57,116 @@ data Phase = All
            | Query
     deriving (Typeable, Data, Eq, Read, Show)
 
-data Decoder = MultiPhase  { mdig   :: M.Map Int String,
-                             mbuild :: M.Map Int String,
-                             mplace :: M.Map Int String,
-                             mquery :: M.Map Int String }
-             | SinglePhase { sdig   :: M.Map LongPixel String,
-                             sbuild :: M.Map LongPixel String,
-                             splace :: M.Map LongPixel String,
-                             squery :: M.Map LongPixel String }
 
-instance ColorConvertible PixelRGBA8 PixelRGBA16 where
-    {-# INLINE promotePixel #-}
-    promotePixel (PixelRGBA8 r g b a) = PixelRGBA16 (fromIntegral r)
-                                                    (fromIntegral g)
-                                                    (fromIntegral b)
-                                                    (fromIntegral a)
+data CommandDictionary = CommandDictionary {
+                         des :: M.Map PixelRGBA8 String
+                       , bld :: M.Map PixelRGBA8 String
+                       , plc :: M.Map PixelRGBA8 String
+                       , qry :: M.Map PixelRGBA8 String }
+
+translate :: CommandDictionary -> Phase -> PixelRGBA8 -> Maybe String
+translate dict Dig key   = M.lookup key (des dict)
+translate dict Build key = M.lookup key (bld dict)
+translate dict Place key = M.lookup key (plc dict)
+translate dict Query key = M.lookup key (qry dict)
+translate _ _ _          = Nothing --might not be the best fallback
+
+
+data ConfigLists = ConfigLists { designate :: [(String,[String])]
+                               , build     :: [(String,[String])]
+                               , place     :: [(String,[String])]
+                               , query     :: [(String,[String])] }
+
+instance FromJSON ConfigLists where
+    parseJSON (Object v) = ConfigLists <$>
+                          v .: "designate" <*>
+                          v .: "build" <*>
+                          v .: "place" <*>
+                          v .: "query"
+    parseJSON _          = mzero
+
+
+constructDict :: L.ByteString -> L.ByteString -> Maybe CommandDictionary
+constructDict alias config = do
+    aliasLists <- decode alias :: Maybe ConfigLists
+    commands <- decode config :: Maybe ConfigLists
+    buildCommandDict (Just aliasLists) (Just commands)
+  where 
+    buildCommandDict :: Maybe ConfigLists -> Maybe ConfigLists -> Maybe CommandDictionary
+    buildCommandDict Nothing _ = Nothing
+    buildCommandDict _ Nothing = Nothing
+    buildCommandDict (Just al) (Just cs) =
+        buildCommandDict' ( expandList (designate al)
+                          , expandList (build al)
+                          , expandList (place al)
+                          , expandList (query al) )
+                          ( expandPixelList (designate cs)
+                          , expandPixelList (build cs)
+                          , expandPixelList (place cs)
+                          , expandPixelList (query cs) )
+
+    --buildCommandDict' :: (a,a,a,a) -> (b,b,b,b) -> Maybe CommandDictionary
+    buildCommandDict' (a,b,c,d) (w,x,y,z) = do 
+       designate' <- genMap a w
+       build'     <- genMap b x
+       place'     <- genMap c y
+       query'     <- genMap d z
+       Just (CommandDictionary designate' build' place' query')
+
+    genMap :: [(String,String)] -> [(PixelRGBA8,String)] -> Maybe (M.Map PixelRGBA8 String)
+    genMap al cs | length (takeWhile pred genList) == 0  = Just (M.fromList $ map noMaybe genList) 
+                 | otherwise = Nothing
+      where genList = map doLookup cs
+            doLookup (pix,str) = (pix,M.lookup str dict)
+            dict = M.fromList al
+
+            pred (_,Nothing) = False
+            pred (_,Just a)  = True
+
+            noMaybe (a,b) = (a,fromJust b)
+
+    expandList :: [(String,[String])] -> [(String,String)]
+    expandList = concatMap (expand . swap)
+
+    expandPixelList :: [(String,[String])] -> [(PixelRGBA8,String)]
+    expandPixelList = map (toPixel) . expandList
+        
+    expand :: ([String],String) -> [(String,String)]
+    expand ([],_) = []
+    expand ((k:ks),val) = (k,val) : expand (ks,val)
+
+    -- TODO: need to make these check for malformed strings
+    toPixel :: (String,String) -> (PixelRGBA8,String)
+    toPixel (key,val) = (keyToPixel key,val)
+
+    -- color representations:
+    -- base ten: <val>:<val>:<val>:<val>
+    -- hex: #<val><val><val><val> or 0x<val><val><val><val>
+    keyToPixel = listToPixel . keyToPixel'
+
+    keyToPixel' key | head key == '#' = parseHex $ tail key
+                    | take 2 key == "0x" = parseHex $ drop 2 key
+                    | otherwise = parse10 key
+      where parseHex [] = []
+            parseHex (a:b:ks) = (fst $ head (readHex (a:[b]))) : parseHex ks
+
+            parse10 [] = []
+            parse10 ks = (read a) : parse10 b
+              where (a,b) = break (== ':') ks
+
+    listToPixel (r:g:b:a:[]) = PixelRGBA8 r g b a
+
+
+deriving instance Ord PixelRGBA8
 
 main = getArgs >>= executeR Main {} >>= \opts ->
     do
-        {-
-        putStrLn $ show (start opts)
-        putStrLn (input opts)
-        putStrLn (output opts)
-        putStrLn $ show (phases opts)
-        putStrLn $ show (singlePhasePNG opts)
-        -}
-        --imgFiles = words input opts
-        decoderStr <- readFile "pngconfig.json"
-        imgStrs <- mapM readFile $ words $ input opts
-        (strs,imgs) <- liftM partitionEithers $ map decodePng imgStrs
+        aliasStr <- L.readFile "alias.json"
+        configStr <- L.readFile "pngconfig_default.json"
+        dict <- return (constructDict aliasStr configStr)
+        imgStrs <- mapM B.readFile $ words $ input opts
+        (strs,imgs) <- return ( partitionEithers $ map decodePng imgStrs )
         putStrLn $ show strs
-
-
-convertMultiPhase :: [Either String DynamicImage] -> L.ByteString -> Maybe Phase ->
-                      Either String BStringTuple
-convertMultiPhase imgStr decStr phase | phase == Nothing = Left "Ivalid phase declaration"
-                                      | strs /= null = Left $ unlines strs
-                                      | decoder == Nothing = Left "Error parsing pngconfig.json"
-                                      | otherwise = convertMultiPhase' imgs (fromJust decoder) (fromJust phase)
-    where (strs,imgs) = partitionEithers $ map decodePng imgStr
-          decoder     = decode decStr :: Maybe (M.Map String LongPixel)
-          convertMultiPhase' = undefined
-
-type LongPixel = (Int,Int,Int,Int)
-type BStringTuple = (L.ByteString,L.ByteString,L.ByteString,L.ByteString)
-
--- color representations:
--- base ten: <val>:<val>:<val>:<val>
--- hex: #<val><val><val><val> or 0x<val><val><val><val>
-
-invert :: (Ord b) => M.Map a b -> M.Map b a
-invert = M.fromList . swap . M.toList
 
 
 parseDig :: [Int] -> (Int -> String) -> Maybe String
@@ -122,27 +181,27 @@ parsePlace commands mapping = undefined
 parseQuery :: [Int] -> (Int -> String) -> Maybe String
 parseQuery commands mapping = undefined
 
-unzipImage4 :: L.ByteString -> Maybe ([Int],[Int],[Int],[Int])
-unzipImage4 str = convert $ decodePng str
-    where convert (Left s) = Nothing
-          convert (Right img) = Just (unzip4 $ imageToList img)
+--unzipImage4 :: L.ByteString -> Maybe ([Int],[Int],[Int],[Int])
+--unzipImage4 str = convert $ decodePng str
+--    where convert (Left s) = Nothing
+--          convert (Right img) = Just (unzip4 $ imageToList img)
 
 
 
-imageToList :: DynamicImage -> Either String [LongPixel]
-imageToList (ImageRGBA8 img) = Right $ reverse $ pixelFold pixPred [] img
-imageToList (ImageRGBA16 img) = Right $ reverse $ pixelFold pixPred [] img
-imageToList _ = Left "Unsupported png format, use RGBA8 or RGBA16 encoding"
+imageToList :: DynamicImage -> (PixelRGBA8 -> String) -> Either String [String]
+imageToList (ImageRGBA8 img) dict = Right $ reverse $ pixelFold pixPred [] img
+  where pixPred acc _ _ pix = (dict pix) : acc
+imageToList _ _ = Left "Unsupported png format, use RGBA8 encoding"
 
 
-pixPred :: (ColorConvertible a  PixelRGBA16) => [LongPixel] -> x -> x -> a -> [LongPixel]
-pixPred acc _ _ pix = (promoteToTuple pix):acc
+--pixPred :: (ColorConvertible a  PixelRGBA16) => [LongPixel] -> x -> x -> a -> [LongPixel]
+---pixPred acc _ _ pix = (promoteToTuple pix):acc
 
-promoteToTuple :: (ColorConvertible pixel PixelRGBA16) => pixel -> LongPixel
-promoteToTuple p = pixelToTuple p' 
-    where p' = (promotePixel p)::PixelRGBA16
+--promoteToTuple :: (ColorConvertible pixel PixelRGBA16) => pixel -> LongPixel
+--promoteToTuple p = pixelToTuple p' 
+--    where p' = (promotePixel p)::PixelRGBA16
 
 
-pixelToTuple :: PixelRGBA16 -> LongPixel
-pixelToTuple (PixelRGBA16 r g b a) = (fromIntegral r, fromIntegral g, fromIntegral b,
-                        fromIntegral a)
+--pixelToTuple :: PixelRGBA16 -> LongPixel
+--pixelToTuple (PixelRGBA16 r g b a) = (fromIntegral r, fromIntegral g, fromIntegral b,
+--                        fromIntegral a)
