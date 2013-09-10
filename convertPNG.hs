a68e7aa4f8f2423b0a9b86f86c1ccd6e698bcbfc
Leif <lgrele@gmail.com>
2013-09-10 13:55:37 -0700

    Moved config parsing into its own module.

--- convertPNG.hs
+++ convertPNG.hs
@@ -4,14 +4,9 @@ DeriveDataTypeable, OverloadedStrings, StandaloneDeriving #-}
 module Main where
 
 import qualified Data.Map.Strict as M
-import Control.Applicative((<$>),(<*>), empty)
-import Numeric
-import Data.Maybe
 import qualified Data.Vector.Storable as V
-import Data.Either
-import Data.Maybe
-import Data.Tuple
-import Data.Aeson
+import Data.Either(partitionEithers,rights)
+import Data.Maybe(isNothing,fromJust)
 import Data.List(unzip,intercalate,intersperse)
 import Codec.Picture.Png(decodePng)
 import Codec.Picture.Types
@@ -20,6 +15,8 @@ import qualified Data.ByteString as B
 import System.Environment(getArgs)
 import System.Console.CmdLib
 import Control.Monad
+
+import Config(CommandDictionary(..),constructDict)
 
 
 data Main = Main { start :: [Int], input :: String,
@@ -161,103 +158,8 @@ data Phase = Dig
     deriving (Typeable, Data, Eq, Read, Show)
 
 
--- the CommandDictionary describes a mapping from pixels to strings
--- it is accessed via the translate function
-data CommandDictionary = CommandDictionary {
-                         des :: M.Map PixelRGBA8 String
-                       , bld :: M.Map PixelRGBA8 String
-                       , plc :: M.Map PixelRGBA8 String
-                       , qry :: M.Map PixelRGBA8 String }
-
 translate :: CommandDictionary -> Phase -> PixelRGBA8 -> Maybe String
 translate dict Dig   key = M.lookup key (des dict)
 translate dict Build key = M.lookup key (bld dict)
 translate dict Place key = M.lookup key (plc dict)
 translate dict Query key = M.lookup key (qry dict)
-
-
-data ConfigLists = ConfigLists { designate :: M.Map String [String]
-                               , build     :: M.Map String [String]
-                               , place     :: M.Map String [String]
-                               , query     :: M.Map String [String] }
-
-instance FromJSON ConfigLists where
-    parseJSON (Object v) = ConfigLists <$>
-                          v .: "designate" <*>
-                          v .: "build" <*>
-                          v .: "place" <*>
-                          v .: "query"
-    parseJSON _          = mzero
-
-
-deriving instance Ord PixelRGBA8
-
-
-constructDict :: L.ByteString -> L.ByteString -> Maybe CommandDictionary
-constructDict alias config = do
-    aliasLists <- decode alias :: Maybe ConfigLists
-    commands <- decode config :: Maybe ConfigLists
-    buildCommandDict (aliasLists) (commands)
-  where 
-    buildCommandDict :: ConfigLists -> ConfigLists -> Maybe CommandDictionary
-    buildCommandDict al cs =
-        buildCommandDict' ( expandList $ M.toList (designate al)
-                          , expandList $ M.toList (build al)
-                          , expandList $ M.toList (place al)
-                          , expandList $ M.toList (query al) )
-                          ( expandPixelList $ M.toList (designate cs)
-                          , expandPixelList $ M.toList (build cs)
-                          , expandPixelList $ M.toList (place cs)
-                          , expandPixelList $ M.toList (query cs) )
-
-    buildCommandDict' (a,b,c,d) (w,x,y,z) = do 
-       designate' <- genMap a w
-       build'     <- genMap b x
-       place'     <- genMap c y
-       query'     <- genMap d z
-       Just (CommandDictionary designate' build' place' query')
-
-    genMap :: [(String,String)] -> [(PixelRGBA8,String)] -> Maybe (M.Map PixelRGBA8 String)
-    genMap _ [] = Just M.empty
-    genMap [] _ = Just M.empty
-    genMap al cs | length (filter pred genList) == 0  = Just (M.fromList $ map noMaybe genList) 
-                 | otherwise = Nothing
-      where genList = map doLookup cs
-            doLookup (pix,str) = (pix,M.lookup str dict)
-            dict = M.fromList al
-
-            pred (_,a) = isNothing a
-
-            noMaybe (a,b) = (a,fromJust b)
-
-    expandList :: [(String,[String])] -> [(String,String)]
-    expandList = concatMap (expand . swap)
-
-    expandPixelList :: [(String,[String])] -> [(PixelRGBA8,String)]
-    expandPixelList = map (toPixel) . expandList
-    
-    -- expand a tuple holding a list of keys and a value into a list of key value pairs    
-    expand :: ([String],String) -> [(String,String)]
-    expand ([],_) = []
-    expand ((k:ks),val) = (k,val) : expand (ks,val)
-
-    -- TODO: need to make these check for malformed strings
-    toPixel :: (String,String) -> (PixelRGBA8,String)
-    toPixel (key,val) = (keyToPixel key,val)
-
-    -- color representations:
-    -- base ten: <val>:<val>:<val>:<val>
-    -- hex: #<val><val><val><val> or 0x<val><val><val><val>
-    keyToPixel = listToPixel . keyToPixel'
-
-    keyToPixel' key | head key == '#' = parseHex $ tail key
-                    | take 2 key == "0x" = parseHex $ drop 2 key
-                    | otherwise = parse10 key
-      where parseHex [] = []
-            parseHex (a:b:ks) = (fst $ head (readHex (a:[b]))) : parseHex ks
-
-            parse10 [] = []
-            parse10 ks = (read a) : parse10 b
-              where (a,b) = break (== ':') ks
-
-    listToPixel (r:g:b:a:[]) = PixelRGBA8 r g b a
